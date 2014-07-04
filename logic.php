1d2e56312b53b5e652ce1bb7f59170830c7c5317
Mike Helly <mike@mikehelly.com>
2014-07-04 19:54:50 -0400

    added logic.php

---
+++ logic.php
@@ -0,0 +1,20 @@
+<?php
+
+$i = 0;
+
+/*
+if($time >= 5 && $time < 11) {
+        $color = '#865f86';
+    }
+    elseif ($time >= 11 && $time < 16) {
+        $color = '#2c87c8';
+    }
+    elseif ($time >= 16 && $time < 20) {
+        $color = '#c7b02f';
+    }
+    else {
+    	$color = '#180629';
+    }
+*/
+
+?>
