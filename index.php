8a11a58b66ad4759fcc306f1a296c31925625163
Mike Helly <mike@mikehelly.com>
2014-07-04 19:54:02 -0400

    added index.php file

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <title>The Unidentifiable Password Generator</title>
+
+    <?php require 'logic.php' ?>
+    
+    <link href="master.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="blackbg">
+
+<img class="lock" src="lock_white.png" />
+
+</div>
+
+</body>
+</html>
