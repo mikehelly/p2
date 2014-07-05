db3be1fc0715ac49c24b3a5fdff663e4faa12266
Mike Helly <mike@mikehelly.com>
2014-07-05 14:35:56 -0400

    logic with number_of_words up to 6

--- logic.php
+++ logic.php
@@ -1,22 +1,122 @@
 <?php
 
-$words = array(
-    'apples',
-    'ranges',
-    'milk',
-    'cream',
-    'long',
-    'table'
-    );
-
-$i = 0;
-
-$random = $words[array_rand($words)];
-
+/*
 while ($i > 0 && $i < 4) {
     echo $random;
     $i++;
 }
+*/
+
+function show_random() {
+
+    $words = array(
+    'apples',
+    'oranges',
+    'milk',
+    'cream',
+    'long',
+    'table',
+    'odd',
+    'uneven',
+    'basketball',
+    'outdoors',
+    'path',
+    'way',
+    'eat',
+    'restaurant',
+    'people',
+    'mister',
+    'wants',
+    'field',
+    'trees',
+    'full',
+    'left',
+    'optimized',
+    'dog',
+    'leaves',
+    'wall',
+    'new',
+    'ghost',
+    'face',
+    'inspector',
+    'deck',
+    'low',
+    'dirty',
+    'party',
+    'yodel',
+    'capital',
+    'cool',
+    'alright',
+    'rich',
+    'program',
+    'gospel',
+    'preach',
+    'master',
+    'lion',
+    'jungle',
+    'city',
+    'town',
+    'wire',
+    'vindicate',
+    'wonderful',
+    'winter',
+    'weasel',
+    'laughter',
+    'link',
+    'smash',
+    'games',
+    'after',
+    'rectangular',
+    'life',
+    'coding',
+    'umbrella',
+    'zebra',
+    'forest',
+    'beaver',
+    'zoo'
+    );
+
+    if( $_POST['number_of_words'] == 1 ) {
+        echo $words[array_rand($words)];
+    }
+    if( $_POST['number_of_words'] == 2 ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+    elseif( $_POST['number_of_words'] == 3 ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+    elseif( $_POST['number_of_words'] == 4 ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+    elseif( $_POST['number_of_words'] == 5 ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+    elseif( $_POST['number_of_words'] == 6 ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+    elseif( $_POST['number_of_words'] == '' ) {
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+        echo $words[array_rand($words)];
+    }
+}
+
 
 /*
 if($time >= 5 && $time < 11) {
