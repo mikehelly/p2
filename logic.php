7093a75c00d3d0621576d11679732d0029765d77
Mike Helly <mike@mikehelly.com>
2014-07-06 13:33:12 -0400

    updated logic, back to if statement, works on pageload

--- logic.php
+++ logic.php
@@ -7,11 +7,15 @@ while ($i > 0 && $i < 4) {
 }
 */
 
-function wn1() {
+if(isset($_POST['number_of_words'])){ $number_of_words = $_POST['number_of_words']; }
+
+function show_random() {
+
+    global $number_of_words;
 
     $words = array(
-    'apple',
-    'orange',
+    'apples',
+    'oranges',
     'milk',
     'cream',
     'long',
@@ -19,7 +23,7 @@ function wn1() {
     'odd',
     'uneven',
     'basketball',
-    'outdoor',
+    'outdoors',
     'path',
     'way',
     'eat',
@@ -28,12 +32,12 @@ function wn1() {
     'mister',
     'wants',
     'field',
-    'tree',
+    'trees',
     'full',
     'left',
     'optimized',
     'dog',
-    'leaf',
+    'leaves',
     'wall',
     'new',
     'ghost',
@@ -73,34 +77,151 @@ function wn1() {
     'zebra',
     'forest',
     'beaver',
-    'zoo',
-    'clean',
-    'type',
-    'skateboard',
-    'album'
+    'zoo'
     );
 
-    
-    
-    echo $words[array_rand($words)];
+$dashwords = array(
+    '-apples',
+    '-oranges',
+    '-milk',
+    '-cream',
+    '-long',
+    '-table',
+    '-odd',
+    '-uneven',
+    '-basketball',
+    '-outdoors',
+    '-path',
+    '-way',
+    '-eat',
+    '-restaurant',
+    '-people',
+    '-mister',
+    '-wants',
+    '-field',
+    '-trees',
+    '-full',
+    '-left',
+    '-optimized',
+    '-dog',
+    '-leaves',
+    '-wall',
+    '-new',
+    '-ghost',
+    '-face',
+    '-inspector',
+    '-deck',
+    '-low',
+    '-dirty',
+    '-party',
+    '-yodel',
+    '-capital',
+    '-cool',
+    '-alright',
+    '-rich',
+    '-program',
+    '-gospel',
+    '-preach',
+    '-master',
+    '-lion',
+    '-jungle',
+    '-city',
+    '-town',
+    '-wire',
+    '-vindicate',
+    '-wonderful',
+    '-winter',
+    '-weasel',
+    '-laughter',
+    '-link',
+    '-smash',
+    '-games',
+    '-after',
+    '-rectangular',
+    '-life',
+    '-coding',
+    '-umbrella',
+    '-zebra',
+    '-forest',
+    '-beaver',
+    '-zoo'
+    );
+
+$numbers = array(
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0'
+    );
+
+    //number of words 1
+    if( $number_of_words == 1 ) {
+        echo $words[array_rand($words)];
+    }
+    //number of words 2
+    elseif( $number_of_words == 2 ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+    }
+    //number of words 3
+    elseif( $number_of_words == 3 ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+    }
+    //number of words 4
+    elseif( $number_of_words == 4 ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+    }
+    //number of words 5
+    elseif( $number_of_words == 5 ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+    }
+    //number of words 6
+    elseif( $number_of_words == 6 ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+    }
+    //number of words empty
+    elseif( $number_of_words == '' ) {
+        echo $words[array_rand($words)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+        echo $dashwords[array_rand($dashwords)];
+    }
 }
 
-function show_random_number() {
 
-    $numbers = array(
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        '0'
-        );
-
-    echo $numbers[array_rand($numbers)];
-}
+/*
+if($time >= 5 && $time < 11) {
+        $color = '#865f86';
+    }
+    elseif ($time >= 11 && $time < 16) {
+        $color = '#2c87c8';
+    }
+    elseif ($time >= 16 && $time < 20) {
+        $color = '#c7b02f';
+    }
+    else {
+    	$color = '#180629';
+    }
+*/
 
 ?>
